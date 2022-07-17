ebd2399252a8ff6336840cee1f4e09cb5fe94f86
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-17 15:06:17 -0300

    Added cors package to app.js ➕

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import './helpers/loadEnv.js';
+import cors from 'cors';
+
 const { PORT } = process.env;
 
 import express from 'express';
@@ -7,6 +9,7 @@ import appRouter from './routes/index.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/v1/', appRouter);
 
