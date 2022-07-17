35d54a128aef5f208a4d0b8c34c8342c62f639e1
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-17 12:11:38 -0300

    Added transaction table migration ➕

---
+++ src/db/migrations/20220717142238_init.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.schema.createTable('transaction', function (table) {
+    table.increments('id').primary();
+    table.string('description').notNullable();
+    table.integer('amount').notNullable();
+    table.integer('type').notNullable().defaultTo(0);
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.schema.dropTableIfExists('transaction');
+};
