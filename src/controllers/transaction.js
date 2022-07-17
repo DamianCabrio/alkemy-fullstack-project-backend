3f1493fe773e9f6efec5fccc2c546776aaaffea5
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-17 12:15:14 -0300

    Added basic transaction controller ➕

---
+++ src/controllers/transaction.js
@@ -0,0 +1,13 @@
+class TransactionController {
+  createTransaction(req, res) {}
+
+  getTransaction(req, res) {}
+
+  getAllTransactions(req, res) {}
+
+  updateTransaction(req, res) {}
+
+  deleteTransaction(req, res) {}
+}
+
+export default new TransactionController();
