f4bc0ca52cbe2a287b736cd3908e6e56e1562d2d
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-18 15:52:28 -0300

    Added users and categories routes to route index ➕

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import transactionsRouter from "./transactions/index.js";
+import categoriesRouter from "./categories/index.js";
+import usersRouter from "./users/index.js";
 
 const router = Router();
 
-router.use("/transaction", transactionsRouter);
+router.use("/transactions", transactionsRouter);
+router.use("/categories", categoriesRouter);
+router.use("/users", usersRouter);
 
 export default router;
