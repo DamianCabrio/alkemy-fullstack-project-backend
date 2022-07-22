822a9445528157ea4d1a74bf96d3c99c1a8f2089
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-22 09:07:42 -0300

    Body errors messages now have better format 📈

--- src/middlewares/apiErrorHandler.js
+++ src/middlewares/apiErrorHandler.js
@@ -3,7 +3,12 @@ import ApiError from '../helpers/ApiError.js';
 
 function apiErrorHandler(err, _req, res, _next) {
   if (err instanceof ApiError) {
-    error(res, err.message, err.statusCode);
+    let errMessage;
+    if (err.message.length > 0) {
+      errMessage = err.message.map((message) => message.msg).join(', ');
+    }
+
+    error(res, errMessage || err.message, err.statusCode);
     return;
   }
   error(res, 'Algo salio mal, por favor vuelva a intentar');
